9ef2763b1e8c093076509e2314df14e680ce05cb
alexintosh <alessio.d@gmail.com>
2019-01-17 16:50:03 +0100

    Adding reactstrap :triangular_ruler:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 import EthereumHDWallet from './class/ethereum/EthereumHDWallet';
 import WatcherTx from './class/WatcherTx';
+import { Button } from 'reactstrap';
 const posAddress = '0xd18a54f89603Fe4301b29EF6a8ab11b9Ba24f139';
 class App extends Component {
 
@@ -26,14 +28,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
+      <Container>
+        <Row>
+          <Col>Dexpay</Col>
+        </Row>
+      </Container>
     );
   }
 }
