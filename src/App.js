a0e18128f4b9f696f5520600332c57f5b583eec9
Parminder Klair <perminder.klair@gmail.com>
2019-01-28 00:33:57 +0530

    removed unneeded files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
 
-import './App.css';
 import EthereumHDWallet from './class/ethereum/EthereumHDWallet';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { Error404, Payment , POS} from './pages';
+import { Error404, Payment , POS } from './pages';
 const posAddress = '0xd18a54f89603Fe4301b29EF6a8ab11b9Ba24f139';
 
-const Dummy = () => {
-  return (<div>Dexpay: POS</div>)
-}
 class App extends Component {
-
   componentDidMount() {
     this.init();
   }
 
   async init() {
-    
     this.wallet = new EthereumHDWallet(false, posAddress);
     await this.wallet.setWeb3();
     await this.wallet.fetchBalance();
