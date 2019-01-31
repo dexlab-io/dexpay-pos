036049cebc8d5481583655075145e6606a29432d
Parminder Klair <perminder.klair@gmail.com>
2019-01-31 16:02:48 +0530

    added seprate views for desktop and mobile

---
+++ src/pages/Dashboard/mobile.view.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+import Layout from '../../components/Layout';
+import Seo from '../../components/Seo';
+import Keypad from './components/Keypad';
+import RecentPayments from './components/RecentPayments';
+
+export default function() {
+  const { activeTab } = this.state;
+
+  return (
+    <Layout>
+      <Seo title="POS" description="POS System" />
+      <section className="section">
+        <div className="container is-fluid">
+          <div className="tabs ">
+            <ul>
+              <li
+                className={
+                  activeTab === 'numberPad'
+                    ? 'is-active has-text-weight-semibold'
+                    : ''
+                }
+              >
+                <a onClick={() => this.setState({ activeTab: 'numberPad' })}>
+                  <Trans>Numberpad</Trans>
+                </a>
+              </li>
+              <li
+                className={
+                  activeTab === 'recentPayments'
+                    ? 'is-active has-text-weight-semibold'
+                    : ''
+                }
+              >
+                <a
+                  onClick={() => this.setState({ activeTab: 'recentPayments' })}
+                >
+                  <Trans>Recent Payments</Trans>
+                </a>
+              </li>
+            </ul>
+          </div>
+          {activeTab === 'numberPad' && (
+            <React.Fragment>
+              <Keypad
+                handleChange={val => this.setState({ totalAmount: val })}
+              />
+              <button
+                type="submit"
+                className="button is-black is-uppercase is-large is-fullwidth"
+                onClick={this.handlePay}
+              >
+                <Trans>Generate bill</Trans>
+              </button>
+            </React.Fragment>
+          )}
+          {activeTab === 'recentPayments' && <RecentPayments />}
+        </div>
+      </section>
+    </Layout>
+  );
+}
