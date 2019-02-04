b71aa78da2edd509881305f6f0418614d2416045
Parminder Klair <perminder.klair@gmail.com>
2019-02-04 12:54:05 +0530

    added watch query example

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
+import apolloClient from '../utils/apolloClient';
 
 const query = gql`
   {
@@ -46,59 +47,67 @@ const Logout = () => (
   </Mutation>
 );
 
-const Test = () => {
-  return (
-    <Layout>
-      <Seo title="Test page" description="A Test page" />
-      <div className="section">
-        <div className="container">
-          <h2 className="title">User data:</h2>
-          <Query query={query} fetchPolicy="cache-and-network">
-            {({ data, loading, error }) => {
-              if (loading) return <p>loading...</p>;
-              if (error && !data.user) return <p>Error: {error.message}</p>;
-              console.log('data.user', data.user);
+class Test extends React.Component {
+  async componentDidMount() {
+    apolloClient.watchQuery({ query }).subscribe(result => {
+      console.log('result', result);
+    });
+  }
 
-              if (data.user) {
-                return (
-                  <div>
-                    ID: {data.user.id}
-                    <br />
-                    Name: {data.user.fullName}
-                    <br />
-                    Email: {data.user.email}
-                    <br />
-                    <br />
-                    <Logout />
-                  </div>
-                );
-              }
-              return <div>Not logged in</div>;
-            }}
-          </Query>
-          <hr />
-          <Mutation mutation={loginMutation}>
-            {(login, { loading, error }) => (
-              <React.Fragment>
-                <button
-                  type="submit"
-                  className={`button is-primary ${loading && 'is-loading'}`}
-                  onClick={() =>
-                    login({
-                      variables: { email: 'abc@abc.com', password: 'abc123' }
-                    })
-                  }
-                >
-                  Login
-                </button>
-                {error && <p>Error: {error.message}</p>}
-              </React.Fragment>
-            )}
-          </Mutation>
+  render() {
+    return (
+      <Layout>
+        <Seo title="Test page" description="A Test page" />
+        <div className="section">
+          <div className="container">
+            <h2 className="title">User data:</h2>
+            <Query query={query} fetchPolicy="cache-and-network">
+              {({ data, loading, error }) => {
+                if (loading) return <p>loading...</p>;
+                if (error && !data.user) return <p>Error: {error.message}</p>;
+                console.log('data.user', data.user);
+
+                if (data.user) {
+                  return (
+                    <div>
+                      ID: {data.user.id}
+                      <br />
+                      Name: {data.user.fullName}
+                      <br />
+                      Email: {data.user.email}
+                      <br />
+                      <br />
+                      <Logout />
+                    </div>
+                  );
+                }
+                return <div>Not logged in</div>;
+              }}
+            </Query>
+            <hr />
+            <Mutation mutation={loginMutation}>
+              {(login, { loading, error }) => (
+                <React.Fragment>
+                  <button
+                    type="submit"
+                    className={`button is-primary ${loading && 'is-loading'}`}
+                    onClick={() =>
+                      login({
+                        variables: { email: 'abc@abc.com', password: 'abc123' }
+                      })
+                    }
+                  >
+                    Login
+                  </button>
+                  {error && <p>Error: {error.message}</p>}
+                </React.Fragment>
+              )}
+            </Mutation>
+          </div>
         </div>
-      </div>
-    </Layout>
-  );
-};
+      </Layout>
+    );
+  }
+}
 
 export default Test;
