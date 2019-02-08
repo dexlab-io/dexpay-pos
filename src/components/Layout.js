6046b388520038fe98b1a967d0fd726b1c44b770
Parminder Klair <perminder.klair@gmail.com>
2019-02-08 12:20:21 +0530

    installed react sidebar package

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,19 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import styled from 'styled-components';
+import Sidebar from 'react-sidebar';
 
 import config from '../config';
 import Header from './Header';
 
 const Container = styled.div``;
 
-const Layout = ({ children, header }) => (
-  <Container>
-    <Helmet title={config.siteName} />
-    <Header {...header} />
-    {children}
-  </Container>
-);
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarOpen: false
+    };
+  }
+
+  onSetSidebarOpen = open => {
+    this.setState({ sidebarOpen: open });
+  };
+
+  render() {
+    const { children, header } = this.props;
+    const { sidebarOpen } = this.state;
+
+    return (
+      <Container>
+        <Helmet title={config.siteName} />
+        <Header {...header} />
+        <Sidebar
+          sidebar={<b>Sidebar content</b>}
+          open={sidebarOpen}
+          onSetOpen={this.onSetSidebarOpen}
+          styles={{ sidebar: { background: 'white' } }}
+        >
+          <button type="button" onClick={() => this.onSetSidebarOpen(true)}>
+            Open sidebar
+          </button>
+        </Sidebar>
+        {children}
+      </Container>
+    );
+  }
+}
 
 Layout.defaultProps = {
   header: {}
