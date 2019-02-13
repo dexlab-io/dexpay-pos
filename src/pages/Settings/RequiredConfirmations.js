0015d6f641cdc3f51917cf416750320ca0bfb2a4
Parminder Klair <perminder.klair@gmail.com>
2019-02-13 07:39:13 +0530

    added required confirmations page

--- src/pages/Settings/RequiredConfirmations.js
+++ src/pages/Settings/RequiredConfirmations.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from '../../components/Layout';
 import Seo from '../../components/Seo';
 import SettingsHeader from './components/SettingsHeader';
 import Breadcrumb from './components/Breadcrumb';
 import { Slider } from '../../components/elements';
+
+const SliderContainer = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  padding: 20px;
+  border-bottom: ${props => `1px solid ${props.theme.borderColor}`};
+`;
+const SliderLabel = styled.span`
+  flex: 2;
+`;
+const SliderValue = styled.span`
+  flex: 1;
+  color: #9c9c9c;
+`;
+const SliderWrapper = styled.div`
+  flex: 10;
+`;
 
 class RequiredConfirmations extends React.Component {
   state = { value: 50 };
@@ -24,11 +43,18 @@ class RequiredConfirmations extends React.Component {
               title="Required Confirmations"
               icon="link-icon.png"
             />
-            <p>RequiredConfirmations slider here</p>
-            <Slider
-              value={value}
-              onChange={e => this.setState({ value: e.target.value })}
-            />
+            <SliderContainer>
+              <SliderLabel className="has-text-weight-semibold">
+                Number of blocks
+              </SliderLabel>
+              <SliderValue>{value}</SliderValue>
+              <SliderWrapper>
+                <Slider
+                  value={value}
+                  onChange={e => this.setState({ value: e.target.value })}
+                />
+              </SliderWrapper>
+            </SliderContainer>
           </div>
         </div>
       </Layout>
