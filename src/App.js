1779e5137999fd30294420fb82f938eca15cd047
Parminder Klair <perminder.klair@gmail.com>
2019-02-28 23:27:58 +0530

    set wallet address from metamask and query var from url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import qs from 'qs';
 import { ThemeProvider } from 'styled-components';
 import { ApolloProvider } from 'react-apollo';
-import { BrowserRouter, Route, Switch, matchPath } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import gql from 'graphql-tag';
 
 import './theme/bulma.css'; // load bulma
 import './localization'; // load i18n
@@ -22,42 +23,48 @@ import {
   Test
 } from './pages';
 
+const updateAddressMutation = gql`
+  mutation updateWalletAddress($address: String!) {
+    updateWalletAddress(address: $address) @client
+  }
+`;
+
 class App extends Component {
   state = { loaded: false };
 
   async componentDidMount() {
-    // await this.init();
     await persistor.restore();
     this.client = client;
     this.setState({ loaded: true });
+    await this.init();
   }
 
   async init() {
-    const t = matchPath(window.location.pathname, {
-      path: '/address/:id'
-    });
+    // const t = matchPath(window.location.pathname, {
+    //   path: '/address/:id'
+    // });
 
-    if (t && t.params && t.params.id) {
-      const address = t.params.id;
-      // store.update.pos.address(address, null, 'GET');
-      return;
-    }
+    // if (t && t.params && t.params.id) {
+    //   const address = t.params.id;
+    //   store.update.pos.address(address, null, 'GET');
+    //   return;
+    // }
 
     const params = qs.parse(window.location.search.slice(1));
     this.wallet = new EthereumHDWallet();
     await this.wallet.setWeb3();
 
-    // if (this.wallet.getAddress()) {
-    //   store.update.pos.address(
-    //     this.wallet.getAddress(),
-    //     null,
-    //     'Injected Provider'
-    //   );
-    // } else if (!this.wallet.getAddress() && params.posAddress) {
-    //   store.update.pos.address(params.posAddress, null, 'GET');
-    // } else {
-    //   store.update.pos.address(null, 'Pos address is empty', null);
-    // }
+    if (this.wallet.getAddress()) {
+      this.client.mutate({
+        mutation: updateAddressMutation,
+        variables: { address: this.wallet.getAddress() }
+      });
+    } else if (params.posAddress) {
+      this.client.mutate({
+        mutation: updateAddressMutation,
+        variables: { address: params.posAddress }
+      });
+    }
   }
 
   render() {
