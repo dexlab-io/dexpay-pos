a2a4e0ad8d6293711f6e00d545a571ae1dca61b1
Parminder Klair <perminder.klair@gmail.com>
2019-03-08 00:05:20 +0530

    removed invoices store

--- src/store/index.js
+++ src/store/index.js
@@ -3,20 +3,16 @@ import { mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import globalType from './global/schema';
 import globalData from './global/resolvers';
 import globalDefaults from './global/defaults';
-import invoiceType from './invoiceData/schema';
-import invoiceData from './invoiceData/resolvers';
-import invoiceDefaults from './invoiceData/defaults';
 import userData from './userData/resolvers';
 import userType from './userData/schema';
 import userDefaults from './userData/defaults';
 
-const resolvers = mergeResolvers([globalData, userData, invoiceData]);
-const typeDefs = mergeTypes([globalType, userType, invoiceType]);
+const resolvers = mergeResolvers([globalData, userData]);
+const typeDefs = mergeTypes([globalType, userType]);
 
 const defaults = {
   ...globalDefaults,
-  ...userDefaults,
-  ...invoiceDefaults
+  ...userDefaults
 };
 
 export { resolvers, typeDefs, defaults };
