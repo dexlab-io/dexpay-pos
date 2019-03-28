072663b0e11352f15009de42459bf9bef8707143
Parminder Klair <perminder.klair@gmail.com>
2019-03-29 00:58:11 +0530

    added components

--- src/pages/Invoice/index.js
+++ src/pages/Invoice/index.js
@@ -9,6 +9,8 @@ import AddressClipboard from '../../components/AddressClipboard';
 import NetworkStatus from './components/NetworkStatus';
 import Breadcrumb from './components/Breadcrumb';
 import dexLogo from '../../assets/images/dex-logo.png';
+import metaMaskLogo from '../../assets/images/metamask-logo.png';
+import starbuckLogo from '../../assets/dummy/starbuck-logo.png';
 
 const Container = styled.div`
   width: 445px;
@@ -52,21 +54,42 @@ const InnerBoxHeader = styled(InnerBoxShared)`
 `;
 const InnerBoxContent = styled(InnerBoxShared)`
   grid-area: content;
+  background-color: #ffffff;
 `;
 const InnerBoxFooter = styled(InnerBoxShared)`
   grid-area: footer;
   background-color: #f0f0f0;
 `;
 
+const BrandLogo = styled.img`
+  max-height: 50px;
+  width: auto;
+  height: auto;
+`;
 const Tag = styled.p`
   font-size: 14px;
 `;
 
+const Button = styled.button`
+  margin: 10px 0px;
+  padding-left: 2em;
+  padding-right: 2em;
+`;
+const ButtonLogo = styled.img`
+  width: 23px;
+  height: auto;
+  margin-right: 12px;
+`;
+
 class Invoice extends React.Component {
-  state = { step: 1 };
+  constructor(props) {
+    super(props);
+
+    this.state = { step: 1, walletConnected: false, isSuccessful: false };
+  }
 
   render() {
-    const { step } = this.state;
+    const { step, walletConnected, isSuccessful } = this.state;
     let stepText = 'Payment Method';
     if (step === 2) {
       stepText = 'Connect Wallet';
@@ -82,26 +105,109 @@ class Invoice extends React.Component {
             <Breadcrumb step={step} text={stepText} />
             <InnerBox>
               <InnerBoxHeader>
-                <span>LOGO HERE</span>
+                <BrandLogo src={starbuckLogo} />
               </InnerBoxHeader>
               <InnerBoxContent>
-                <Tag>Total you pay</Tag>
-                <FiatAmount fiatAmount={parseFloat('14.50')} />
-                <CryptoAmount
-                  cryptoCurrency="dai"
-                  cryptoValue={12}
-                  fiatAmount={parseFloat('14.50')}
-                  hasSelection
-                  handleChange={option => {
-                    console.log({ selectedCurrency: option.value });
-                  }}
-                />
-                <button
-                  type="submit"
-                  className="button is-black is-large is-fullwidth"
-                >
-                  CONTINUE
-                </button>
+                {step === 1 && (
+                  <React.Fragment>
+                    <Tag>Total you pay</Tag>
+                    <FiatAmount fiatAmount={parseFloat('14.50')} />
+                    <CryptoAmount
+                      cryptoCurrency="dai"
+                      cryptoValue={12}
+                      fiatAmount={parseFloat('14.50')}
+                      hasSelection
+                      handleChange={option => {
+                        console.log({ selectedCurrency: option.value });
+                      }}
+                    />
+                    <button
+                      type="submit"
+                      className="button is-black is-large is-fullwidth"
+                    >
+                      CONTINUE
+                    </button>
+                  </React.Fragment>
+                )}
+                {step === 2 && !walletConnected && (
+                  <React.Fragment>
+                    <Tag>Unlock your wallet</Tag>
+                    <Button
+                      type="button"
+                      onClick={this.handleUseMetamask}
+                      className="button is-large is-fullwidth"
+                    >
+                      <ButtonLogo src={metaMaskLogo} alt="Metamask Logo" />
+                      METAMASK
+                    </Button>
+                  </React.Fragment>
+                )}
+                {step === 2 && walletConnected && (
+                  <React.Fragment>
+                    <Tag>Total you pay</Tag>
+                    <FiatAmount fiatAmount={parseFloat('14.50')} />
+                    <CryptoAmount
+                      cryptoCurrency="dai"
+                      cryptoValue={12}
+                      fiatAmount={parseFloat('14.50')}
+                      hasSelection={false}
+                    />
+                    <p>
+                      You need to give permission for Dexpay to interact with
+                      DAI
+                    </p>
+                    <button
+                      type="submit"
+                      className="button is-black is-large is-fullwidth"
+                    >
+                      APPROVE
+                    </button>
+                  </React.Fragment>
+                )}
+                {step === 3 && !isSuccessful && (
+                  <React.Fragment>
+                    <Tag>Total you pay</Tag>
+                    <FiatAmount fiatAmount={parseFloat('14.50')} />
+                    <CryptoAmount
+                      cryptoCurrency="dai"
+                      cryptoValue={12}
+                      fiatAmount={parseFloat('14.50')}
+                      hasSelection={false}
+                    />
+                    <p>
+                      You need to give permission for Dexpay to interact with
+                      DAI
+                    </p>
+                    <button
+                      type="submit"
+                      className="button is-black is-large is-fullwidth"
+                    >
+                      PAY NOW
+                    </button>
+                  </React.Fragment>
+                )}
+                {step === 3 && isSuccessful && (
+                  <React.Fragment>
+                    <Tag>You paid!</Tag>
+                    <FiatAmount fiatAmount={parseFloat('14.50')} />
+                    <CryptoAmount
+                      cryptoCurrency="dai"
+                      cryptoValue={12}
+                      fiatAmount={parseFloat('14.50')}
+                      hasSelection={false}
+                    />
+                    <p>
+                      You need to give permission for Dexpay to interact with
+                      DAI
+                    </p>
+                    <button
+                      type="submit"
+                      className="button is-large is-fullwidth"
+                    >
+                      See it on Etherscan
+                    </button>
+                  </React.Fragment>
+                )}
               </InnerBoxContent>
               <InnerBoxFooter>
                 <AddressClipboard address="0xcgffd3h53dfsdf43g405c3647f9c2599...4j2I" />
