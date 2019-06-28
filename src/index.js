98a063899c5f18d4a21d9f3e43a959792ab700b1
Parminder Klair <perminder.klair@gmail.com>
2019-06-28 18:22:57 +0530

    added sentry error tracking

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 
 import App from './App';
 // import config from './config';
 import * as serviceWorker from './utils/serviceWorker';
+
+Sentry.init({
+  dsn: 'https://e8b127fff69b4cd1a4cc10f1b0c67377@sentry.io/1492683'
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
